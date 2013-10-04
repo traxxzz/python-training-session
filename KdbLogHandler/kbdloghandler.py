fa3802e51c124f6fa8b82c2568a12f1d1b3530b7
traxxzz <unixoidfreak@gmail.com>
2013-10-04 01:10:34 -0700

    Defined signature of handler classes. Updated docs.

--- KdbLogHandler/kbdloghandler.py
+++ KdbLogHandler/kbdloghandler.py
@@ -1,4 +1,4 @@
-"""Handler for getting historical market data from KDB(Mercury) via parsing dump files in csv format.
+"""Handler for getting historical market data from KDB(Mercury) via deserialization of dump files.
 
 Instantiate appropriate class with filename. Returned object acts like a
 dictionary, with key-value pairs for each piece of market data.
@@ -7,8 +7,21 @@ dictionary, with key-value pairs for each piece of market data.
 
 
 Handler can be extended by adding classes for particular markets, e.g.
-...
+NYSEKbdLogHandler, BATSKbdLogHandler etc. Each class is completely responsible for
+parsing its files appropriately; see NSDQFileInfo for example.
 """
 
-
 __author__ = 'abbath'
+
+class KbdLogHandler(dict):
+    "store file market data"
+    def __init__(self, filename=None):
+        self["name"] = filename
+
+class NSDQKbdLogHandler(KbdLogHandler):
+    "store market data from dump file in csv format of MARKET"
+    def _parse(self, filename):
+        "parse market data from dump file"
+        self.clear()
+        for line in file(filename):
+            pass
